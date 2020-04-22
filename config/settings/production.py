fb642e89ae07427a9d4e70214637c6db7746ee67
julio <julio336@hotmail.com>
2020-04-22 13:35:28 -0500

    pushing to Heroku

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY=os.environ.get('SECRET_KEY')
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["example.com"])
+ALLOWED_HOSTS = ["conciente.herokuapp.com"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
